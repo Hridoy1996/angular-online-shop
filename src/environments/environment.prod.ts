f51a6a4cd05c08f5fcd6ae8c79a602bc19322172
TM Hridoy <hridoytareqmahmud@gmail.com>
2024-07-10 12:04:37 +0600

    chore: add base components

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+      apiKey: "AIzaSyD9ijJj0sA4udDnQm6YO7BXE8_w3oYPm2I",
+      authDomain: "jiboneprothom-2ce9e.firebaseapp.com",
+      projectId: "jiboneprothom-2ce9e",
+      storageBucket: "jiboneprothom-2ce9e.appspot.com",
+      messagingSenderId: "523348517325",
+      appId: "1:523348517325:web:5d62e87aa7c82c6ded2484",
+      measurementId: "G-9CVH57HYN3"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+      apiKey: "AIzaSyD9ijJj0sA4udDnQm6YO7BXE8_w3oYPm2I",
+      authDomain: "jiboneprothom-2ce9e.firebaseapp.com",
+      projectId: "jiboneprothom-2ce9e",
+      storageBucket: "jiboneprothom-2ce9e.appspot.com",
+      messagingSenderId: "523348517325",
+      appId: "1:523348517325:web:5d62e87aa7c82c6ded2484",
+      measurementId: "G-9CVH57HYN3"
+  }
 };
 
 /*
